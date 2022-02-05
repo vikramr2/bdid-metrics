7f745e035ac7c802c71b2af2bdeedf192927cbe3
Franklin Moy <franklinmoy3@gmail.com>
2022-02-05 11:58:00 -0600

    Made the view creation more readable

--- franklin/bu_et_al_2021/create_view_cp_between_100_1000.sql
+++ franklin/bu_et_al_2021/create_view_cp_between_100_1000.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW fmoy3.exosome_pubs_with_cp_between_100_1000 AS
-    SELECT cited_integer_id, COUNT(*) AS cp
+    SELECT cited_integer_id, COUNT(citing_integer_id) AS cp
     FROM dimensions.exosome_1900_2010_sabpq_deduplicated
     GROUP BY cited_integer_id
-    HAVING COUNT(*) > 100 AND COUNT(*) < 1000
+    HAVING COUNT(citing_integer_id) > 100 AND COUNT(citing_integer_id) < 1000
     ORDER BY cp DESC;
