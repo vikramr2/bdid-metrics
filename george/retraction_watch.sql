1348f712e6b5a79fc9251240c0764bbe4f4f6389
George Chacko <chackoge@users.noreply.github.com>
2022-03-05 14:20:55 -0600

    added TERM_YR as derived from TERM_CD

--- george/retraction_watch.sql
+++ george/retraction_watch.sql
@@ -1,6 +1,6 @@
 -- integer_ids from exosome data that are in the retraction_doi list
 
-SELECT retraction_doi,edcn.integer_id,edcn.year FROM cleaned_rw cr
+SELECT distinct retraction_doi,edcn.integer_id,edcn.year FROM cleaned_rw cr
 INNER JOIN dimensions.exosome_dimensions_complete_nodelist edcn
   ON edcn.doi=lower(cr.retraction_doi);
 
@@ -12,5 +12,16 @@ SELECT edcn.integer_id,edcn.year,
 FROM cleaned_rw cr
 INNER JOIN dimensions.exosome_dimensions_complete_nodelist edcn
   ON edcn.doi=lower(cr.original_paper_doi))
-select retraction_year-orig_year from cte;
+select distinct retraction_year,orig_year,original_paper_doi from cte;
 
+SELECT MAX(record_id),original_paper_doi, COUNT(DISTINCT record_id)
+FROM cleaned_rw
+WHERE original_paper_doi != 'Unavailable'
+OR original_paper_doi != 'unavailable'
+GROUP BY original_paper_doi
+HAVING COUNT(DISTINCT record_id) > 1;
+
+SELECT retraction_doi,original_paper_doi, max(record_id) OVER (PARTITION BY original_paper_doi) as record
+FROM cleaned_rw where original_paper_doi='10.1001/archpediatrics.2012.999'
+
+select count(record_id) from cleaned_rw where retraction_doi is not null;
