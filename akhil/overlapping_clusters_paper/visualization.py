ad280b33422afc70893a650551f190ed4249246c
Akhil Jakatdar <akhilrj2@chackoge-serv01.cs.illinois.edu>
2022-04-26 09:46:15 -0500

    Updated OKMP README.md

--- akhil/overlapping_clusters_paper/visualization.py
+++ akhil/overlapping_clusters_paper/visualization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
+import statistics
 
 
 def scatterplot_analysis(information, x_col, x_scale, y_col, y_scale, hue_val, output_file_path):
@@ -17,8 +17,10 @@ def scatterplot_analysis(information, x_col, x_scale, y_col, y_scale, hue_val, o
 
 def histogram_analysis(frequency_list, output_file_path, min_k_core, inclusion_criterion):
   plt.figure()
-  print(min(frequency_list), max(frequency_list), sum(frequency_list)/len(frequency_list))
-  sns.displot(frequency_list, kde=False, bins=100)
+  print(min(frequency_list), max(frequency_list), statistics.median(frequency_list))
+  ax = sns.displot(frequency_list, kde=False, bins=100)
+  ax.set_xscale('log')
+  ax.set_yscale('log')
   plt.xlabel('Num Clusters Placed Into')
   plt.ylabel('Frequency')
   plt.savefig(str(min_k_core) + '_' + str(inclusion_criterion) + '_'  + output_file_path)
